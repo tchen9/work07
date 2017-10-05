6085805e8893c6eff091ca0b43489220450129e3
Tina <tchen9@stuy.edu>
2017-10-05 07:10:10 -0400

    finished

--- echo.py
+++ echo.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 app.secret_key = "this is not secure"
 
 @app.route('/')
 def home():
-	return render_template('form.html', title = 'Main')
+        if 'user' in session:
+                return render_template("input.html", user = session['user'], method = request.method)
+        else:
+	        return render_template('form.html', title = 'Main')
 
 @app.route('/welcomePage', methods=['POST','GET'])
 def welcome():
 	print request.form['user']
         user = request.form['user']
-        if user == 'hello':
+        if user == 'bob':
                 if request.form['password'] == 'secret':
+                        session['user'] = request.form['user']
                         return render_template('input.html', title = user + "'s", user = user, method = request.method)
                 else:
                         return render_template('fail.html', message = "Sorry, wrong password")
         else:
                 return render_template('fail.html', message = "Sorry, wrong username")
+
+@app.route('/logout')
+def logout():
+        session.pop('user')
+        return render_template('form.html')
 
 if __name__ == "__main__":
 	app.debug = True
